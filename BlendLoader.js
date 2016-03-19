10d09c622b38210b31c44a63402906e391d469f7
theSynapse <carl@areait.com.au>
2016-03-19 18:26:20 +1100

    Added a basic check to see whether or not the file is gzipped

--- BlendLoader.js
+++ BlendLoader.js
@@ -7,19 +7,40 @@ var BlendLoader = {
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
         var response = xhttp.response;
-        var responseView = new Uint8Array(response);
+        window.responseView = new Uint8Array(response);
 
-        console.log("Now decompressing gzipped .blend");
+        console.log("Checking to see if the .blend is gzipped");
 
-        var gunzip = new Zlib.Gunzip(responseView);
-        window.data = gunzip.decompress();
+        var isCompressed = !BlendLoader.checkHeader(responseView);
 
-        console.log(".blend file successfully loaded and decompressed");
+        if (isCompressed) {
+          console.log(".blend is compressed, now decompressing");
+
+          var gunzip = new Zlib.Gunzip(responseView);
+          window.data = gunzip.decompress();
+
+          console.log(".blend file successfully decompressed");
+        } else {
+          window.data = responseView;
+          
+          console.log(".blend is not compressed, continuing");
+        }
       } else {
         console.log(".blend loader XHTTP status: " + xhttp.readyState + ", " + xhttp.status);
       }
     }
 
     xhttp.send();
+  },
+
+  // Returns true if the header is a valid .blend header, false otherwise
+  checkHeader: function(data) {
+    return data[0] == 66 &&
+           data[1] == 76 &&
+           data[2] == 69 &&
+           data[3] == 78 &&
+           data[4] == 68 &&
+           data[5] == 69 &&
+           data[6] == 82;
   }
 }
