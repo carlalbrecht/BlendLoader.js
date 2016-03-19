4d19338d50653799e98a50eb35abf0d06ef6022f
theSynapse <carl@areait.com.au>
2016-03-19 18:33:50 +1100

    Added some basic checks to see whether or not you are actually loading a .blend file

--- BlendLoader.js
+++ BlendLoader.js
@@ -6,25 +6,7 @@ var BlendLoader = {
 
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
-        var response = xhttp.response;
-        window.responseView = new Uint8Array(response);
-
-        console.log("Checking to see if the .blend is gzipped");
-
-        var isCompressed = !BlendLoader.checkHeader(responseView);
-
-        if (isCompressed) {
-          console.log(".blend is compressed, now decompressing");
-
-          var gunzip = new Zlib.Gunzip(responseView);
-          window.data = gunzip.decompress();
-
-          console.log(".blend file successfully decompressed");
-        } else {
-          window.data = responseView;
-          
-          console.log(".blend is not compressed, continuing");
-        }
+        BlendLoader.parseReturnData(xhttp.response);
       } else {
         console.log(".blend loader XHTTP status: " + xhttp.readyState + ", " + xhttp.status);
       }
@@ -32,6 +14,40 @@ var BlendLoader = {
 
     xhttp.send();
   },
+
+  parseReturnData: function(response) {
+    window.responseView = new Uint8Array(response);
+
+    console.log("Checking to see if the .blend is gzipped");
+
+    var isCompressed = !BlendLoader.checkHeader(responseView);
+
+    if (isCompressed) {
+      console.log(".blend is compressed, now decompressing");
+
+      try {
+        var gunzip = new Zlib.Gunzip(responseView);
+        window.data = gunzip.decompress();
+      } catch (err) {
+        console.warn("Supplied URL is either malformed or is not a .blend file");
+        console.error(err);
+        return false;
+      }
+
+      var isActuallyBlend = BlendLoader.checkHeader(data);
+
+      if (isActuallyBlend) {
+        console.log(".blend file successfully decompressed");
+      } else {
+        console.warn("Supplied URL was gzipped, but is not a .blend file");
+        return false;
+      }
+    } else {
+      window.data = responseView;
+
+      console.log(".blend is not compressed, continuing");
+    }
+  }
 
   // Returns true if the header is a valid .blend header, false otherwise
   checkHeader: function(data) {
