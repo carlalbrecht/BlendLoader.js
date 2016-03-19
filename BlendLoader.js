a589c705cd5a6f1f34eba26e193cfcea87000269
theSynapse <carl@areait.com.au>
2016-03-19 20:22:40 +1100

    Switched to OO and made the loader use the THREE namespace convention

--- BlendLoader.js
+++ BlendLoader.js
@@ -1,45 +1,62 @@
-var BlendLoader = {
-  load: function(URL) {
-    var xhttp = new XMLHttpRequest()
-    xhttp.open("GET", URL, true);
-    xhttp.responseType = "arraybuffer";
+/**
+ * @author Carl Albrecht / http://thesynapse.github.io/
+ */
 
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState == 4 && xhttp.status == 200) {
-        BlendLoader.parseReturnData(xhttp.response);
-      } else {
-        console.log(".blend loader XHTTP status: " + xhttp.readyState + ", " + xhttp.status);
-      }
+(function(){
+
+THREE.BlendLoader = function() {
+
+}
+
+THREE.BlendLoader.prototype = new THREE.Loader();
+THREE.BlendLoader.prototype.constructor = THREE.BlendLoader;
+
+THREE.BlendLoader.prototype.load = function(url, onLoad, onProgress, onError) {
+  // Get the .blend from the supplied URL
+  var xhttp = new XMLHttpRequest()
+  xhttp.open("GET", url, true);
+  xhttp.responseType = "arraybuffer";
+
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      parseReturnData(xhttp.response);
+    } else {
+      console.warn(".blend loader XHTTP status: " + xhttp.readyState + ", " + xhttp.status);
     }
+  }
 
-    xhttp.send();
-  },
+  xhttp.send();
 
-  parseReturnData: function(response) {
+  // Called once the XMLHttpRequest finishes doing its thing
+  function parseReturnData(response) {
     window.responseView = new Uint8Array(response);
 
     console.log("Checking to see if the .blend is gzipped");
 
-    var isCompressed = !BlendLoader.checkHeader(responseView);
+    var isCompressed = !checkHeader(responseView);
 
     if (isCompressed) {
       console.log(".blend is compressed, now decompressing");
 
       try {
+        console.time(".blend decompression");
         var gunzip = new Zlib.Gunzip(responseView);
         window.data = gunzip.decompress();
       } catch (err) {
         console.warn("Supplied URL is either malformed or is not a .blend file");
+        console.timeEnd(".blend decompression");
         console.error(err);
         return false;
       }
 
-      var isActuallyBlend = BlendLoader.checkHeader(data);
+      var isActuallyBlend = checkHeader(data);
 
       if (isActuallyBlend) {
         console.log(".blend file successfully decompressed");
+        console.timeEnd(".blend decompression");
       } else {
         console.warn("Supplied URL was gzipped, but is not a .blend file");
+        console.timeEnd(".blend decompression");
         return false;
       }
     } else {
@@ -49,8 +66,8 @@ var BlendLoader = {
     }
   }
 
-  // Returns true if the header is a valid .blend header, false otherwise
-  checkHeader: function(data) {
+  // Returns true if the first 7 bytes of the supplied data spell BLENDER
+  function checkHeader(data) {
     return data[0] == 66 &&
            data[1] == 76 &&
            data[2] == 69 &&
@@ -60,3 +77,5 @@ var BlendLoader = {
            data[6] == 82;
   }
 }
+
+})();
